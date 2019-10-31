dad6ecae270f87403e6f0e18f2aaa906b6c0881e
Isaac Karth <isaac@isaackarth.com>
2019-10-31 00:27:43 -0700

    hooking up the visualiations

--- wfc/wfc_visualize.py
+++ wfc/wfc_visualize.py
@@ -1,8 +1,10 @@
 "Visualize the patterns into tiles and so on."
 
 import matplotlib.pyplot as plt
+import numpy as np
 import imageio
-
+import math
+import pathlib
 
 
 ## Helper functions
@@ -67,26 +69,55 @@ def tile_grid_to_image(tile_grid, tile_catalog, tile_size, visualize=False, part
     return new_img
 
 
+def figure_list_of_tiles(unique_tiles, tile_catalog, fname="list_of_tiles.pdf"):
+  plt.figure(figsize=(4,4), edgecolor='k', frameon=True)
+  plt.title('Extracted Tiles')
+  s = math.ceil(math.sqrt(len(unique_tiles)))+1
+  for i,tcode in enumerate(unique_tiles[0]):
+    sp = plt.subplot(s, s, i + 1).imshow(tile_catalog[tcode])
+    sp.axes.tick_params(labelleft=False, labelbottom=False, length=0)
+    plt.title(f"{i} : {tcode}", fontsize=10)
+    sp.axes.grid(False)
+  fp = pathlib.Path(fname)
+  plt.savefig(fp, bbox_inches="tight")
+  plt.close()
 
-def image_to_disk(image, filename):
-    return
+def figure_false_color_tile_grid(tile_grid, filename="./false_color_tiles.png"):
+    figure_plot = plt.matshow(tile_grid, cmap='gist_ncar',extent=(0, tile_grid.shape[1], tile_grid.shape[0], 0))
+    plt.title('False Color Map of Tiles in Input Image');
+    figure_plot.axes.grid(None)
+    plt.savefig(filename, bbox_inches="tight")
+    plt.close()
+    
 
 
-def figure_list_of_tiles(unique_tiles, tile_catalog, tile_ids):
-    figure(figsize=(4,4), edgecolor='k', frameon=True)
-    title('Extracted Tiles')
-    s = math.ceil(math.sqrt(len(unique_tiles)))+1
-    for tcode,i in tile_ids.items():
-        sp = subplot(s, s, i + 1).imshow(tile_catalog[tcode])
-        sp.axes.tick_params(labelleft=False, labelbottom=False, length=0)
-        title(i, fontsize=10)
-        sp.axes.grid(False)
 
+def render_pattern(render_pattern, tile_catalog):
+    rp_iter = np.nditer(render_pattern, flags=['multi_index'])
+    output = np.zeros(render_pattern.shape + (3,), dtype=np.uint32)
+    while not rp_iter.finished:
+        # Note that this truncates images with more than 3 channels down to just the channels in the output.
+        # If we want to have alpha channels, we'll need a different way to handle this.
+        output[rp_iter.multi_index] = np.resize(tile_catalog[render_pattern[rp_iter.multi_index]], output[rp_iter.multi_index].shape)
+        rp_iter.iternext()     
+    return output
+
+def figure_pattern_catalog(pattern_catalog, tile_catalog, pattern_weights, pattern_width, output_filename="pattern_catalog"):
+    s_columns = 24 // min(24, pattern_width)
+    s_rows = 1 + (int(len(pattern_catalog)) // s_columns)
+    fig = plt.figure(figsize=(s_columns, s_rows * 1.5))
+    plt.title('Extracted Patterns')
+    counter = 0
+    for i, tcode in pattern_catalog.items():
+        pat_cat = pattern_catalog[i]
+        ptr = render_pattern(pat_cat, tile_catalog).astype(np.uint8)
+        sp = plt.subplot(s_rows, s_columns, counter + 1)
+        spi = sp.imshow(ptr)
+        spi.axes.xaxis.set_label_text(f'({pattern_weights[i]})')
+        sp.set_title(f"{counter}")
+        spi.axes.tick_params(labelleft=False,labelbottom=False, left=False, bottom=False)
+        spi.axes.grid(False)
+        counter += 1
+    plt.savefig(output_filename + "_patterns.pdf", bbox_inches="tight")
     plt.close()
 
-def figure_false_color_tile_grid(tile_grid):
-    figure_plot = matshow(tile_grid, cmap='gist_ncar',extent=(0, tile_grid.shape[1], tile_grid.shape[0], 0))
-    title('False Color Map of Tiles in Input Image');
-    figure_plot.axes.grid(None)
-    p1t.close()
-    
