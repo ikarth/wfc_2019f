81b617bdbdea87d2984b4a114860a9d9e28d58f5
Valentin Valls <valentin.valls@gmail.com>
2020-06-07 12:09:45 +0200

    Rework setup with pyproject.toml

--- setup.py
+++ setup.py
@@ -1,29 +1,6 @@
-import os
-from setuptools import setup
+#!/usr/bin/env python
 
-PROJECT_DIR = os.path.dirname(__file__)
+import setuptools
 
-def read(fname):
-    return open(os.path.join(PROJECT_DIR, fname)).read()
-
-
-version = '0.1'
-
-
-setup(
-    name="wfc_python",
-    version=version,
-    author = "Isaac Karth",
-    author_email = "isaac@isaackarth.com",
-    description = "Implementation of wave function collapse in Python",
-    license = "MIT License",
-    keywords = "sample wfc wave-function-collapse",
-    url = "https://github.com/ikarth/wfc_python",
-    packages=['wfc', 'tests'],
-    long_description=read('README.md'),
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Topic :: Utilities",
-        "License :: OSI Approved :: MIT License",
-    ],
-)
+if __name__ == "__main__":
+    setuptools.setup()
